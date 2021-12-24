f02a922a37eb5b2ffc38731ad7ce15192101a98b
Br@ini@c R@wkib <brainiacrawkib@gmail.com>
2021-12-24 10:01:19 -0800

    :hammer: added CreateRoomSerializer

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .serializers import RoomSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
 
 
