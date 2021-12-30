96caa6655364c46a96659c90ad0ea8f2f477587f
Br@ini@c R@wkib <brainiacrawkib@gmail.com>
2021-12-30 08:19:15 -0800

    :hammer: edited views.py

--- spotify/views.py
+++ spotify/views.py
@@ -6,6 +6,10 @@ from rest_framework import status
 from rest_framework.response import Response
 
 
+REDIRECT_URI = settings.REDIRECT_URI
+CLIENT_ID = settings.CLIENT_ID
+CLIENT_SECRET = settings.CLIENT_SECRET
+
 class AuthURL(APIView):
     def get(self, request, format=None):
         scopes = "user-read-playback-state user-modify-playback-state user-read-currently-playing"
@@ -14,8 +18,22 @@ class AuthURL(APIView):
         data = {
             'scope': scopes,
             'response_type': 'code',
-            'redirect_uri': settings.REDIRECT_URI,
-            'client_id': settings.CLIENT_ID
+            'redirect_uri': REDIRECT_URI,
+            'client_id': CLIENT_ID
         }
         url = Request('GET', spotify_url, params=data).prepare().url
         return Response({'url': url}, status=status.HTTP_200_OK)
+
+
+def spotify_callback(request, format=None):
+    code = request.GET.get('code')
+    error = request.GET.get('error')
+    url = 'https://accounts.spotify.com/api/token'
+    payload = {
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': REDIRECT_URI,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
+    }
+    response = post(url, data=payload).json()
