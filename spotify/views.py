cf017d40efdec83bc6169756cf5706699d469bf6
Br@ini@c R@wkib <brainiacrawkib@gmail.com>
2021-12-31 17:45:06 -0800

    :hamme

--- spotify/views.py
+++ spotify/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 from rest_framework.views import APIView
 from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
+from .utils import update_or_create_user_tokens
 
 
 REDIRECT_URI = settings.REDIRECT_URI
@@ -44,3 +45,13 @@ def spotify_callback(request, format=None):
     refresh_token = response.get('refresh_token')
     expires_in = response.get('expires_in')
     error = response.get('error')
+
+    if not request.session.exists(request.session.session_key):
+        request.session.create()
+
+    update_or_create_user_tokens(request.session.session_key,
+                                 access_token,
+                                 token_type,
+                                 expires_in,
+                                 refresh_token)
+    return redirect('frontend:')
