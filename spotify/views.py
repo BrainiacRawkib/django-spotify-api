8fcced096d9d114896c796e9021d693d4e9809cf
Br@ini@c R@wkib <brainiacrawkib@gmail.com>
2021-12-31 17:45:43 -0800

    :hammer: views.py

--- spotify/views.py
+++ spotify/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.conf import settings
 from rest_framework.views import APIView
 from requests import Request, post
