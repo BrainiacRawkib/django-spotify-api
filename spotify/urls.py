887e28483806a0ff54b68343d3ceffadef5753f4
Br@ini@c R@wkib <brainiacrawkib@gmail.com>
2022-01-01 17:44:58 -0800

    :hammer: added url path for CurrentSong() view

--- spotify/urls.py
+++ spotify/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('get-auth-url/', views.AuthURL.as_view()),
     path('redirect/', views.spotify_callback),
     path('is-authenticated/', views.IsAuthenticated.as_view()),
+    path('current-song/', views.CurrentSong.as_view()),
 ]
