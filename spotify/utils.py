5e84927c09b8390bd79fa27adb9a6cfb9f0f53a4
Br@ini@c R@wkib <brainiacrawkib@gmail.com>
2022-01-01 17:37:07 -0800

    :hammer: execute_spotify_api_request()

--- spotify/utils.py
+++ spotify/utils.py
@@ -1,9 +1,10 @@
 from datetime import timedelta
 from django.conf import settings
 from django.utils import timezone
-from requests import post
+from requests import post, put, get
 from .models import SpotifyToken
 
+BASE_URL = 'https://api.spotify.com/v1/me/'
 REDIRECT_URI = settings.REDIRECT_URI
 CLIENT_ID = settings.CLIENT_ID
 CLIENT_SECRET = settings.CLIENT_SECRET
@@ -69,3 +70,19 @@ def refresh_spotify_token(session_id):
     refresh_token = response.get('refresh_token')
 
     update_or_create_user_tokens(session_id, access_token, token_type, expires_in, refresh_token)
+
+
+def execute_spotify_api_request(session_id, endpoint, post_=False, put_=False):
+    tokens = get_user_tokens(session_id)
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + tokens.access_token}
+
+    if post_:
+        post(BASE_URL + endpoint, headers=headers)
+    if put_:
+        put(BASE_URL + endpoint, headers=headers)
+
+    response = get(BASE_URL + endpoint, {}, headers=headers)
+    try:
+        return response.json()
+    except:
+        return {'Error': 'Issue with request'}
