3032778db255a88a987696c3c0ae040c7411bd73
Br@ini@c R@wkib <brainiacrawkib@gmail.com>
2022-01-04 14:33:22 -0800

    :hammer: edited HomePage.js

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -6,6 +6,7 @@ import {Grid, Button, ButtonGroup, Typography} from "@material-ui/core";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
 import RoomJoinPage from "./RoomJoinPage";
+import Info from "./Info";
 
 export default class HomePage extends Component{
     constructor(props) {
@@ -40,10 +41,13 @@ export default class HomePage extends Component{
                 </Grid>
                 <Grid item xs={12} align="center">
                     <ButtonGroup disableElevation variant="contained" color="primary">
-                        <Button color="primary" to="/join" component={Link}>
+                        <Button color="primary" to="/join/" component={Link}>
                             Join a Room
                         </Button>
-                        <Button color="secondary" to="/create" component={Link}>
+                        <Button color="default" to="/info/" component={Link}>
+                            Info
+                        </Button>
+                        <Button color="secondary" to="/create/" component={Link}>
                             Create a Room
                         </Button>
                     </ButtonGroup>
@@ -62,8 +66,9 @@ export default class HomePage extends Component{
                                 (this.renderHomePage())}
                         }
                     />
-                    <Route path="/join" component={RoomJoinPage} />
-                    <Route path="/create" component={CreateRoomPage} />
+                    <Route path="/join/" component={RoomJoinPage} />
+                    <Route path="/info/" component={Info} />
+                    <Route path="/create/" component={CreateRoomPage} />
                     <Route path="/room/:roomCode"
                            render={(props) => {
                                return <Room {...props} leaveRoomCallback={this.clearRoomCode} />
