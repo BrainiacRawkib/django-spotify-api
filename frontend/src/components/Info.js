ef403ea1db8eded959b38dc77a9fa99ecaa3d11d
Br@ini@c R@wkib <brainiacrawkib@gmail.com>
2022-01-04 16:00:46 -0800

    :hammer: edited Info.js

--- frontend/src/components/Info.js
+++ frontend/src/components/Info.js
@@ -5,12 +5,46 @@ import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
 import NavigateNextIcon from "@material-ui/icons/NavigateNext";
 
 export default function Info(props) {
+    const pages = {
+        JOIN: 'pages.join',
+        CREATE: 'pages.create',
+    }
+    const [page, setPage] = useState(pages.JOIN);
+
+    useEffect(() => {
+        console.log('It worked!');
+        return () => console.log('clean up!')
+    })
+
+    function joinInfo() {
+        return 'Join page';
+    }
+
+    function createInfo() {
+        return 'Create page';
+    }
+
     return (
         <Grid container spacing={1}>
             <Grid item xs={12} align='center'>
                 <Typography component='h4' variant='h4'>
                     What Django Spotify API?
                 </Typography>
+            </Grid>
+            <Grid item xs={12} align='center'>
+                <Typography variant='body1'>
+                    {page === pages.JOIN ? joinInfo() : createInfo()}
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align='center'>
+                <IconButton onClick={() => {page === pages.CREATE ? setPage(pages.JOIN) : setPage(pages.CREATE)}}>
+                    {page === pages.CREATE ? (<NavigateBeforeIcon />) : (<NavigateNextIcon />)}
+                </IconButton>
+            </Grid>
+            <Grid item xs={12} align='center'>
+                <Button color='secondary' variant='contained' to="/" component={Link}>
+                    Back
+                </Button>
             </Grid>
         </Grid>
     )
